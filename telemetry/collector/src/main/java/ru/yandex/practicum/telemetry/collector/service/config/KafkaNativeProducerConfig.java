d0f489f2e8582df1b271f5302713ba3a6471e820
LyutovSergey <psvr2020@yandex.ru>
2026-06-19 19:30:47 +0300

    rev 1.1 переход на kafka-clients

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/config/KafkaNativeProducerConfig.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.telemetry.collector.service.config;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
+
+import java.util.Properties;
+
+@Configuration
+public class KafkaNativeProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean(destroyMethod = "close")
+    public Producer<String, SpecificRecordBase> nativeKafkaProducer() {
+        Properties properties = new Properties();
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+
+        Serializer<String> keySerializer = new StringSerializer();
+        Serializer<SpecificRecordBase> valueSerializer = new GeneralAvroSerializer();
+
+        return new KafkaProducer<>(properties, keySerializer, valueSerializer);
+    }
+}
--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/kafka/KafkaEventProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/kafka/KafkaEventProducer.java
@@ -1,25 +1,30 @@
 package ru.yandex.practicum.telemetry.collector.service.kafka;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class KafkaEventProducer {
-
-    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     @Value("${kafka.topic.sensor-events}")
     private String sensorEventsTopic;
 
     @Value("${kafka.topic.hub-events}")
     private String hubEventsTopic;
+
+    private final Producer<String, SpecificRecordBase> kafkaProducer;
+
+    public KafkaEventProducer(Producer<String, SpecificRecordBase> kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
 
     public void sendSensorEvent(SensorEventAvro event) {
         sendEvent(sensorEventsTopic, event.getId(), event);
@@ -29,16 +34,17 @@ public class KafkaEventProducer {
         sendEvent(hubEventsTopic, event.getHubId(), event);
     }
 
-    private void sendEvent(String topic, String key, Object event) {
+    private void sendEvent(String topic, String key, SpecificRecordBase  event) {
         log.debug("Sending event to topic {}: {}", topic, event);
-        kafkaTemplate.send(topic, key, event)
-                .whenComplete((result, ex) -> {
-                    if (ex == null) {
-                        log.debug("Successfully sent event to topic {}: offset={}",
-                                topic, result.getRecordMetadata().offset());
-                    } else {
-                        log.error("Failed to send event to topic {}", topic, ex);
-                    }
-                });
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(topic, key, event);
+        kafkaProducer.send(record, (RecordMetadata metadata, Exception ex) -> {
+            if (ex == null) {
+                log.debug("Successfully sent event to topic {}: offset={}",
+                        topic, metadata.offset());
+            } else {
+                // Если GeneralAvroSerializer выбросит SerializationException, она также придет сюда в 'ex'
+                log.error("Failed to send event to topic {}", topic, ex);
+            }
+        });
     }
 }
